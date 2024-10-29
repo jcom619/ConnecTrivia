15ceef461170474bbce37e4e2091355a32b0fb37
Joshua <joshuacompton@my.smccd.edu>
2024-10-29 04:50:00 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,34 @@
+// const btn = document.getElementById("modalBtn");
+
+// const popShow = document.getElementById("thisModal");
+
+// const closePop = document.querySelector(".close");
+
+
+
+
+
+
+// // Get all the buttons you want to target
+// const buttons = document.querySelectorAll('.my-button');
+
+// // Add the same event listener to each button
+// buttons.forEach(button => {
+//   btn.addEventListener('click', () => {
+//     popShow.classList.toggle("active");
+//     console.log('Button clicked!'); 
+//   });
+// });
 const btn = document.getElementById("modalBtn");
 
 const popShow = document.getElementById("thisModal");
 
 const closePop = document.querySelector(".close");
 
+btn.addEventListener("click", function () {
+  popShow.classList.add("active");
+});
 
-
-
-
-
-// Get all the buttons you want to target
-const buttons = document.querySelectorAll('.my-button');
-
-// Add the same event listener to each button
-buttons.forEach(button => {
-  btn.addEventListener('click', () => {
-    popShow.classList.toggle("active");
-    console.log('Button clicked!'); 
-  });
+closePop.addEventListener("click", function () {
+  popShow.classList.remove("active");
 });
